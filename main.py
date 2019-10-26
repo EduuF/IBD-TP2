d239bee3618fb6b2b132f582f9447dffab598bd6
Eduardo Fiuza Cruz <eduardofiuza6@hotmail.com>
2019-10-25 23:04:39 -0300

    Incluindo Base de dados reais

--- main.py
+++ main.py
@@ -1,49 +1,15 @@
-from recomendacao import avaliacoes
-from math import sqrt
+#NÃO COLOQUEI 'IMPORT *' P DESCREVER O Q CADA FUNÇAO FAZ
+from funcoes import getRecomendacao ##Retorna o filme mais recomendado para o usuário
+from funcoes import getSimilares ##Retorna, em ordem, os outros usuarios com perfil mais similares
+from funcoes import carrega_movielens ##carrega a base de dados
 
-##############CALCULA DISTANCIA EUCLIDIANA#########################
-
-def euclidiana (usuario1, usuario2):
-    si = {}
-
-    for item in avaliacoes[usuario1]:
-        if item in avaliacoes[usuario2]:
-            si[item] = 1
-    if len(si) == 0:
-        return 0 
-
-    soma = sum([pow(avaliacoes[usuario1][item] - avaliacoes[usuario2][item], 2) 
-                for item in avaliacoes[usuario1] if item in avaliacoes[usuario2] ])
-
-    return 1/(1+sqrt(soma))
-
-def getSimilares(usuario):
-    similaridade = [(euclidiana(usuario, outro), outro) for outro in avaliacoes if outro != usuario]
-    similaridade.sort() ##Ordena em ordem crescente
-    similaridade.reverse() ##Inverte para os maiores virem primeiro
-    return similaridade
-
-def getRecomendacao(usuario):
-    totais ={}
-    somaSimilaridade={}
-    for outro in avaliacoes:
-        if outro == usuario: continue ##Não compara consigo mesmo
-        similaridade = euclidiana(usuario, outro)
-
-        if similaridade <= 0: continue ##Se não houver similaridade não conta
-
-        for item in avaliacoes[outro]:
-            if item not in avaliacoes[usuario]: ##Pega só os outros usuarios que assistiram
-                totais.setdefault(item, 0)
-                totais[item] += avaliacoes[outro][item] * similaridade ##multiplica a avaliação com o peso
-                somaSimilaridade.setdefault(item,0)
-                somaSimilaridade[item] += similaridade ##guarda os pesos
-
-    rankings = [(total/somaSimilaridade[item], item) for item, total in totais.items()]
-    rankings.sort()
-    rankings.reverse()
-    return rankings
+############BASE DE DADOS#############
+from recomendacao_filmes import avaliacoes_filmes
+from recomendacao_usuarios import avaliacoes_usuario
 
 
-#print(getSimilares('Pedro'))
-print(getRecomendacao('Leonardo'))
+####################MAIN######################
+
+base = carrega_movielens() ##carrega a base de dados
+print(getRecomendacao(base, '100'))
+print(getSimilares(base, '1'))
